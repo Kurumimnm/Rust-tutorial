679cd8dfe8978a7ac3f56d3d5b495a2b3613976f
Krumi15 <2220397@ecc.ac.jp>
2024-04-22 15:44:25 +0900

    create Hello World

---
+++ hello_world/target/debug/deps/hello_world.d
@@ -0,0 +1,5 @@
+C:\Users\2220397\Desktop\Git Hub\Rust-tutorial\hello_world\target\debug\deps\hello_world.exe: src\main.rs
+
+C:\Users\2220397\Desktop\Git Hub\Rust-tutorial\hello_world\target\debug\deps\hello_world.d: src\main.rs
+
+src\main.rs:
---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+C:\Users\2220397\Desktop\Git\ Hub\Rust-tutorial\hello_world\target\debug\hello_world.exe: C:\Users\2220397\Desktop\Git\ Hub\Rust-tutorial\hello_world\src\main.rs
